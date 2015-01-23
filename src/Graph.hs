25a53e66d79fbb636c33e44a764f99dbf9ae48b2
Ting-Yen Lai <banacorn@gmail.com>
2015-01-24 03:30:21 +0800

    Sparse Graph <=> Dense Graph

--- src/Graph.hs
+++ src/Graph.hs
@@ -1,7 +1,11 @@
-module Graph (
-        -- module Graph.Dense
-    -- ,   module Graph.Sparse
-) where
+module Graph where
 
--- import Graph.Dense
--- import Graph.Sparse
+import qualified Graph.Dense as D
+import qualified Graph.Sparse as S
+
+toSparse :: D.Graph -> S.Graph
+toSparse (D.Graph xs) = S.Graph (map D.toVertex xs)
+
+fromSparse :: S.Graph -> D.Graph
+fromSparse (S.Graph xs) = D.Graph (map fromVertex xs)
+    where   fromVertex = foldl D.insert (replicate (length xs) False)
